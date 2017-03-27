9a890eee3af211ce1b277b48fabbf397b908da65
Vitaliy Tim <post.vitaliy@gmail.com>
2017-03-27 15:11:29 +0300

    initial setup

---
+++ milo test/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  milo test
+//
+//  Created by Vitaliy Tim on 3/27/17.
+//  Copyright © 2017 Vitaliy Timoshenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // MARK: - Actions
+    
+    @IBAction func test1Tapped(_ sender: UIButton) {
+        print("test 1")
+    }
+    
+    @IBAction func test2Tapped(_ sender: UIButton) {
+        print("test 2")
+    }
+    
+    @IBAction func test3Tapped(_ sender: UIButton) {
+        print("test 3")
+    }
+
+}
+
